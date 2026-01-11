5c7b01c0903583e38bd9099fe9e432d9b1b3fe3c
Nate60 <natealdred60@gmail.com>
2026-01-10 22:48:35 -0500

    fixed curSearch colour

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd({ "ColorScheme", "VimEnter" }, {
+    group = vim.api.nvim_create_augroup("Color", {}),
+    pattern = "*",
+    callback = function()
+        vim.api.nvim_set_hl(0, "CurSearch", { bg = "#AADADA", fg = "#113434" })
+    end,
+})
